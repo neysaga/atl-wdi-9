602154f2c3a0d9ee24f3598a5c3110870157d4fa
neysa21 <neysa21@gmail.com>
2017-03-07 14:59:31 -0500

    jeopardy project

---
+++ main.js
@@ -0,0 +1,8 @@
+// var newElement = document.createElement("p");
+// newElement.textContent = `I wish I liked ${response} too. Sometimes, it is sad to be a computer. :( `;
+// document.body.appendChild(newElement);
+window.onload = function() {
+}
+function(button 1) {
+  		console.log('you have clicked button one');
+  	}
