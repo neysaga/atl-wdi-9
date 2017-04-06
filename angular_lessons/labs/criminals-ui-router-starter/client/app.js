327213e2f809e93440d268a58b919040c3f2a322
neysa21 <neysa21@gmail.com>
2017-04-06 13:04:47 -0400

    lab progress

--- angular_lessons/labs/criminals-ui-router-starter/client/app.js
+++ angular_lessons/labs/criminals-ui-router-starter/client/app.js
@@ -1,3 +1,24 @@
 const angular = require('angular');
+require('angular-ui-router');
 
-angular.module('criminals', []);
+angular
+	.module('criminals', ['ui.router'])
+	.config(uiRouterSetup)
+
+uiRouterSetup.$inject = ['$stateProvider', '$urlRouterProvider'];
+function uiRouterSetup($stateProvider, $urlRouterProvider) {
+	$stateProvider
+		.state('home', {
+			url: '/',
+			template: '<h1>Home</h1>'
+		})
+		.state('about', {
+			url: '/about',
+			template: '<h1>About</h1>'
+		})
+		.state('criminals', {
+			url: '/',
+			template: '<h1>Criminals</h1>'
+		})
+	$urlRouterProvider.otherwise('/');
+}
